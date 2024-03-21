68b3e2d9b7ce88b9e696168bf655798777e003d9
Sanghitam101 <164131140+Sanghitam101@users.noreply.github.com>
2024-03-21 10:35:54 +0800

    Create Slow.py

---
+++ Slow.py
@@ -0,0 +1,13 @@
+
+users = {'omfar1': '1231', 'omfar2': '1232', 'omfar3'>
+
+def login():
+    username = input("MASUKAN NAMA ANDA : ")
+    password = input("MASUKAN PASSWORD ANDA : ")
+
+    if username in users and users[username] == passw>
+        print("SELAMAT DATADNG")
+    else:
+        print("Invalid username or password. Please t>
+
+
